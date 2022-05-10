696907b53972bda7bb542812a5896b8783e35e88
Shahzaib <m.shahzaib.123456@gmail.com>
2022-05-10 15:33:56 +0500

    description updated in app js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var userObject={
     cellNumber:"+92 (349) 0956508",
     email:"nomankhan.ks95@gmail.com",
 
-    userReview1:"I am Certified from(Aptech Global Learning solutions Malaysia) Information  Technolgy (Pvt) batch 2017 and also i am a  IT Consultant in Fieldway Technology Pvt and working on  CA broadcom Infrastructure Managment and Security Managment since 2017 and i am giving my best todo something in technology.",
-    userReview2:"I have completed my schooling from Sarfaraz Academy primary and Secondry karachi  in 2014 as 1<sup>st</sup> division. then i completed my Inter from Mumtaz Govt boys and Girls collage karachi  as 2<sup>st</sup> division in 2014 from the begining i was very passionate about computer technologies therefore studying in Aptech Learning soloution is a <strong> Golden Chance </strong> for me. I'll never try to loose this and always be sincere with those who give me this chance and be always with them for development of country.",
+    userReview1:"I am Certified from(Aptech Global Learning solutions Malaysia) Information  Technolgy (Pvt) batch 2017 and also i have more than 2 years experince in Ionic Framework. I have developed and designed more than 10 Projects",
+    userReview2:"I have completed my schooling from Al Hadeed Grammar High School and Secondry karachi  in 2012. then i completed my Inter from Govt. Islamia Science  Collage Karachi  in 2015 from the begining i was very passionate about computer technologies therefore studying in Aptech Learning soloution is a <strong> Golden Chance </strong> for me. I'll never try to loose this and always be sincere with those who give me this chance and be always with them for development of country.",
     userQuote:"<i>Make your Way to Success</br> The Glory Will be yours</i>"
 }
 
